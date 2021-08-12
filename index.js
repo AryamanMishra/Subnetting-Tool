bd5ff2add98e2941bb6b0248220abb1bfa0be76d
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-08-12 11:23:19 +0530

    subnet mask calc. added

--- index.js
+++ index.js
@@ -24,32 +24,40 @@ app.get('/', (req,res) => {
     res.render('home')
 })
 
+function bin_to_dec(binary) {
+    let decimal = 0;
+    for (let i = binary.length-1;i>=0;i--) 
+        decimal += (1<<(binary.length -  i - 1))*(binary.charAt(i) - '0')
+    return decimal
+}
 
-
-function give_subnet(CIDR_range) {
+function give_subnet_mask(CIDR_range) {
     const slash = CIDR_range.indexOf('/')
     let cidr = CIDR_range.substring(slash+1)
-    let subnet_mask = '';
+    let subnet_mask_bin = '';
+    let subnet_mask_bin_copy = '';
+    let subnet_mask_dec = '';
     cidr = parseInt(cidr)
-    let no_255 = cidr/8;
-    if (cidr % 8 == 0) {
-        for (let i=0;i<no_255;i++)
-            subnet_mask += '255.'
+    for (let i=0;i<3;i++)
+        subnet_mask_bin += "00000000."
+    subnet_mask_bin += "00000000";
+    for (let i=0;i<subnet_mask_bin.length-3;i++) {
+        if (i < cidr)
+            subnet_mask_bin_copy += '1';
+        else
+            subnet_mask_bin_copy += '0';
     }
-    else {
-        for (let i=0;i<no_255-1;i++)
-            subnet_mask += '255.'
-    }
-    let diff = 32 - cidr;
-    diff = 1<<diff;
-    subnet_mask += (256-diff).toString();
-    return subnet_mask
+    subnet_mask_dec += bin_to_dec(subnet_mask_bin_copy.substring(0,8)) + ".";
+    subnet_mask_dec += bin_to_dec(subnet_mask_bin_copy.substring(8,16)) + ".";
+    subnet_mask_dec += bin_to_dec(subnet_mask_bin_copy.substring(16,24)) + ".";
+    subnet_mask_dec += bin_to_dec(subnet_mask_bin_copy.substring(24));
+    return subnet_mask_dec
 }
 
 app.post('/', (req,res) => {
     let CIDR_range = req.body.CIDR_range
-    let subnet = give_subnet(CIDR_range)
-    res.render('output', {CIDR_range,subnet})
+    let subnet_mask = give_subnet_mask(CIDR_range)
+    res.render('output', {CIDR_range,subnet_mask})
 })
 
 
