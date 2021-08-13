4f3001f0b87b9f33661515e7ac469873d8bbf784
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-08-13 10:04:52 +0530

    updated ids

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-A tool which can divide a given network into its subnets while giving details about the network ID(start IP),
-broadcast ID(last IP), number of hosts and the subnet masks of the subnets.
-It requires 2 inputs namely, the CIDR range(network id/prefix of subnet mask of network) and the number of subnets you want to divide the network into.
+A tool which gives details about the network ID(start IP),
+broadcast ID(last IP), number of hosts and the subnet mask of the given network
+It requires 1 input , the CIDR range(network id/prefix of subnet mask of network) 
--- index.js
+++ index.js
@@ -18,11 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 
-let table = {
-    "Number_of_subnets" : [1,2,4,8,16,32,64,128,256],
-    "Number_of_hosts" : [256,128,64,32,16,8,4,2,1],
-    "Subnet_mask" : ['/24','/25','/26','/27','/28','/29','/30','/31','/32']
-}
+// let table = {
+//     "Number_of_subnets" : [1,2,4,8,16,32,64,128,256],
+//     "Number_of_hosts" : [256,128,64,32,16,8,4,2,1],
+//     "Subnet_mask" : ['/24','/25','/26','/27','/28','/29','/30','/31','/32']
+// }
 
 app.get('/', (req,res) => {
     res.render('home')
@@ -33,6 +33,14 @@ function bin_to_dec(binary) {
     for (let i = binary.length-1;i>=0;i--) 
         decimal += (1<<(binary.length -  i - 1))*(binary.charAt(i) - '0')
     return decimal
+}
+
+function dec_to_bin(decimal) {
+    let ans =  Number(decimal).toString(2)
+    let x = ans.length
+    for (let i=0;i<8-x;i++)
+        ans = '0' + ans
+    return ans
 }
 
 function give_subnet_mask(CIDR_range) {
@@ -58,54 +66,54 @@ function give_subnet_mask(CIDR_range) {
     return [subnet_mask_dec,subnet_mask_bin_copy]
 }
 
-function divide_in_subnets(CIDR_range, number_of_subnets) {
-    let index = 0;
-    let arr_subnets = table.Number_of_subnets
-    for (let i=0;i<arr_subnets.length;i++) {
-        if (arr_subnets[i] >= number_of_subnets) {
-            index = i;
-            break;
-        }
-    }
-    const slash = CIDR_range.indexOf('/')
-    let starting_network_id = CIDR_range.substring(0,slash)
-    let ans = {}
-    for (let i=0;i<32;i++) {
-        let arr = [];
-        if (i === 0) {
-            arr[0] = starting_network_id
-            arr[1] = table.Subnet_mask[index]
-            arr[2] = parseInt(table.Number_of_hosts[index]) - 2
-            let last_dot_string_number  = parseInt(starting_network_id.substring(10))
-            last_dot_string_number += arr[2] + 1
-            arr[3] = starting_network_id.substring(0,9) + "." + last_dot_string_number;
-            starting_network_id = arr[3]
-            if (parseInt(starting_network_id.substring(10)) > 255) {
-                ans[i] = 0
-                break
-            }
-        }
-        else {
-            let last_dot_string_number  = parseInt(starting_network_id.substring(10))
-            last_dot_string_number += 1
-            if (last_dot_string_number > 255 || last_dot_string_number + ans[0][2] + 1 > 255)
-                break
-            arr[0] = starting_network_id.substring(0,9) + "." + last_dot_string_number;
-            arr[1] = table.Subnet_mask[index]
-            arr[2] = parseInt(table.Number_of_hosts[index]) - 2
-            arr[3] = starting_network_id.substring(0,9) + "." 
-            let x = parseInt(arr[0].substring(10)) + arr[2] + 1
-            arr[3] += x
-            starting_network_id = arr[3]
-            if (parseInt(starting_network_id.substring(10)) > 255) {
-                ans[i] = null
-                break
-            }
-        }
-        ans[i] = arr 
-    }
-    return ans
-}
+// function divide_in_subnets(CIDR_range, number_of_subnets) {
+//     let index = 0;
+//     let arr_subnets = table.Number_of_subnets
+//     for (let i=0;i<arr_subnets.length;i++) {
+//         if (arr_subnets[i] >= number_of_subnets) {
+//             index = i;
+//             break;
+//         }
+//     }
+//     const slash = CIDR_range.indexOf('/')
+//     let starting_network_id = CIDR_range.substring(0,slash)
+//     let ans = {}
+//     for (let i=0;i<32;i++) {
+//         let arr = [];
+//         if (i === 0) {
+//             arr[0] = starting_network_id
+//             arr[1] = table.Subnet_mask[index]
+//             arr[2] = parseInt(table.Number_of_hosts[index]) - 2
+//             let last_dot_string_number  = parseInt(starting_network_id.substring(10))
+//             last_dot_string_number += arr[2] + 1
+//             arr[3] = starting_network_id.substring(0,9) + "." + last_dot_string_number;
+//             starting_network_id = arr[3]
+//             if (parseInt(starting_network_id.substring(10)) > 255) {
+//                 ans[i] = 0
+//                 break
+//             }
+//         }
+//         else {
+//             let last_dot_string_number  = parseInt(starting_network_id.substring(10))
+//             last_dot_string_number += 1
+//             if (last_dot_string_number > 255 || last_dot_string_number + ans[0][2] + 1 > 255)
+//                 break
+//             arr[0] = starting_network_id.substring(0,9) + "." + last_dot_string_number;
+//             arr[1] = table.Subnet_mask[index]
+//             arr[2] = parseInt(table.Number_of_hosts[index]) - 2
+//             arr[3] = starting_network_id.substring(0,9) + "." 
+//             let x = parseInt(arr[0].substring(10)) + arr[2] + 1
+//             arr[3] += x
+//             starting_network_id = arr[3]
+//             if (parseInt(starting_network_id.substring(10)) > 255) {
+//                 ans[i] = null
+//                 break
+//             }
+//         }
+//         ans[i] = arr 
+//     }
+//     return ans
+// }
 
 function give_number_of_hosts(binary_string) {
     let hosts = 0,zeroes = 0;
@@ -118,35 +126,105 @@ function give_number_of_hosts(binary_string) {
 }
 
 
+function give_network_id(CIDR_range,net_mask) {
+    let ans = ''
+    let index = -1,c = 0
+    for (let i=0;i<CIDR_range.length;i++) {
+        if (CIDR_range.charAt(i) === '/') {
+            index = i
+            break;
+        }
+    }
+    let net_id = CIDR_range.substring(0,index)
+    let binary_net_id = ''
+    let binary_net_mask = ''
+    for (let i=0;i<net_id.length;i++) {
+        if (net_id.charAt(i) === '.') {
+            binary_net_id += dec_to_bin(net_id.substring(c,i)) + '.'
+            c = i+1;
+        }
+    }
+    binary_net_id += dec_to_bin(net_id.substring(c))
+    c = 0
+    for (let i=0;i<net_mask.length;i++) {
+        if (net_id.charAt(i) === '.') {
+            binary_net_mask += dec_to_bin(net_mask.substring(c,i)) + '.'
+            c = i+1;
+        }
+    }
+    binary_net_mask += dec_to_bin(net_mask.substring(c))
+    for (let i=0;i<binary_net_id.length;i++) {
+        if (binary_net_id.charAt(i) === '.')
+            ans += '.'
+        else
+            ans += ((binary_net_id.charAt(i)) & (binary_net_mask.charAt(i)))
+    }
+    c = 0
+    let final_ans = ''
+    for (let i=0;i<ans.length;i++) {
+        if (ans.charAt(i) === '.') {
+            final_ans += bin_to_dec(ans.substring(c,i)) + '.'
+            c = i+1
+        }
+    }
+    final_ans += bin_to_dec(ans.substring(c))
+    return final_ans;
+}
+function give_broadcast_id(CIDR_range,net_mask) {
+    let net_id = give_network_id(CIDR_range,net_mask)
+    let binary_net_id = ''
+    let c = 0
+    for (let i=0;i<net_id.length;i++) {
+        if (net_id.charAt(i) === '.') {
+            binary_net_id += dec_to_bin(net_id.substring(c,i)) + '.'
+            c = i+1
+        }
+    }
+    binary_net_id += dec_to_bin(net_id.substring(c))
+    c = 0
+    let binary_net_mask = '',ans = ''
+    for (let i=0;i<net_mask.length;i++) {
+        if (net_mask.charAt(i) === '.') {
+            binary_net_mask += dec_to_bin(net_mask.substring(c,i)) + '.'
+            c = i+1;
+        }
+    }
+    binary_net_mask += dec_to_bin(net_mask.substring(c))
+    let binary_net_mask_actual = ''
+    for (let i=0;i<binary_net_mask.length;i++) {
+        if (binary_net_mask.charAt(i) === '.')
+            binary_net_mask_actual += '.'
+        else if (binary_net_mask.charAt(i) === '0')
+            binary_net_mask_actual += '1' 
+        else
+            binary_net_mask_actual += '0' 
+    }
+    for (let i=0;i<binary_net_id.length;i++) {
+        if (binary_net_id.charAt(i) === '.')
+            ans += '.'
+        else
+            ans += ((binary_net_id.charAt(i)) | (binary_net_mask_actual.charAt(i)))
+    }
+    c = 0
+    let final_ans = ''
+    for (let i=0;i<ans.length;i++) {
+        if (ans.charAt(i) === '.') {
+            final_ans += bin_to_dec(ans.substring(c,i)) + '.'
+            c = i+1
+        }
+    }
+    final_ans += bin_to_dec(ans.substring(c))
+    return final_ans
+}
+
 app.post('/', (req,res) => {
     let CIDR_range = req.body.CIDR_range
-    let number_of_subnets = req.body.no_of_subnets
-    let index = -1,index_of_last_dot = -1
-    for (let i=0;i<CIDR_range.length;i++) {
-        if (CIDR_range.charAt(i) === '/')
-            index = i
-    }
-    for (let i=0;i<CIDR_range.length;i++) {
-        if (CIDR_range.charAt(i) === '.')
-            index_of_last_dot = i
-    }
-    let network_id_of_CIDR = CIDR_range.substring(0,index)
     let subnet_mask = give_subnet_mask(CIDR_range)
-    let subnets_table = divide_in_subnets(CIDR_range,number_of_subnets)
+    let network_id_of_CIDR = give_network_id(CIDR_range,subnet_mask[0])
+    //let subnets_table = divide_in_subnets(CIDR_range,number_of_subnets)
+    let broadcast_id = give_broadcast_id(CIDR_range,subnet_mask[0])
     let Number_of_hosts = give_number_of_hosts(subnet_mask[1])
-    if (Object.keys(subnets_table).length <= 1) {
-        let broadcast_id = CIDR_range.substring(0,index_of_last_dot)
-        broadcast_id += '.'
-        let host_id = CIDR_range.substring(index_of_last_dot+1,index)
-        host_id = parseInt(host_id)
-        host_id += Number_of_hosts + 1;
-        broadcast_id += host_id
-        res.render('no_subnets', {subnet_mask,Number_of_hosts,network_id_of_CIDR,CIDR_range,broadcast_id})
-    }
-    else if (number_of_subnets <= Object.keys(subnets_table).length)
-        res.render('output', {CIDR_range,subnet_mask,subnets_table,number_of_subnets,Number_of_hosts,network_id_of_CIDR})
-    else
-        res.send('invalid number of subnets')
+    res.render('output', {CIDR_range,subnet_mask,Number_of_hosts,network_id_of_CIDR,broadcast_id})
 })
 
 
